b917d6fefdc88ba3830d537bc3a296b22b78743e
Linaq <dominik.owsianka@gmail.com>
2021-03-08 21:52:50 +0100

    Add project files.

---
+++ LinaqPDF_Tests/PdfDocumentTests.cs
@@ -0,0 +1,21 @@
+﻿using LinaqPDF;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinaqPDF_Tests
+{
+    [TestClass]
+    public class PdfDocumentTests
+    {
+        [TestMethod]
+        public void SaveToFileTest()
+        {
+            PdfDocument doc = new PdfDocument();
+            doc.SaveToFile("test.pdf");
+        }
+    }
+}
